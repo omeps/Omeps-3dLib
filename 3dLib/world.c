b4228323edfde961643ce5f3f8d6edadf5a3654d
omeps <143241382+omeps@users.noreply.github.com>
2023-09-12 15:24:27 -0700

    Update world.c

--- 3dLib/world.c
+++ 3dLib/world.c
@@ -7,17 +7,21 @@ typedef struct sortingkey {
     int index;
     float val;
 } key;
-float min(float a, float b, float c) {
+float max(float a, float b, float c) {
     float s;
     if(b > a) s = b; else s = a;
     if(c > s) return c; else return s;
+};
+float min(float a, float b, float c) {
+    float s;
+    if(b < a) s = b; else s = a;
+    if(c < s) return c; else return s;
 };
 void swap(key *a, key *b) {
     key stored = *a;
     *a = *b;
     *b = stored;    
 };
-
 void sort(int length, key *objects) {
     if(length == 0 || length == 1) {
         return;
@@ -107,25 +111,60 @@ void freeUp(obj o) {
 };
 void addObject(obj object, scr *screen) {
     key *keys = (key *)malloc(sizeof(key)*object.amtOfTriangles);
+    float *maxList = (float *)malloc(sizeof(float)*object.amtOfTriangles);
     for(int i = 0; i < object.amtOfTriangles; i++) {
         (keys+i)->index = i;
         (keys+i)->val = min((*(object.verts+((*(object.triangles+i))[0])))[2],(*(object.verts+((*(object.triangles+i))[1])))[2],(*(object.verts+((*(object.triangles+i))[2])))[2]);
+        *(maxList+i) = max((*(object.verts+((*(object.triangles+i))[0])))[2],(*(object.verts+((*(object.triangles+i))[1])))[2],(*(object.verts+((*(object.triangles+i))[2])))[2]);
+        printf("min: %f, max: %f\n", (keys+i)->val, *(maxList+i));
     }
     sort(object.amtOfTriangles, keys);
-    int start = 0;
-    while(start < object.amtOfTriangles && (keys+start)->val < 0) start++; 
-    for(int i = 0; i < object.amtOfTriangles; i++) {
-        int x[3];
-        int y[3];
-        for(int j = 0; j < 3; j++) {
-            x[j] = (int)(screen->length * (*(object.verts+((*(object.triangles+((keys+i)->index)))[j])))[0]/(*(object.verts+((*(object.triangles+((keys+i)->index)))[j])))[2])+(screen->length>>1);
-            y[j] = (int)(screen->width * (*(object.verts+((*(object.triangles+((keys+i)->index)))[j])))[1]/(*(object.verts+((*(object.triangles+((keys+i)->index)))[j])))[2])+(screen->width>>1);
+    float currentMaxValue = *(maxList + (keys + object.amtOfTriangles - 1)->index);
+    bool zCull[object.amtOfTriangles - 1]; 
+    for(int i = object.amtOfTriangles - 2; i >= 0; i--) {
+        zCull[i] = currentMaxValue > (keys + i)->val;
+        printf("%d, %f, %f\n", zCull[i], currentMaxValue, (keys + i)->val);
+        if(currentMaxValue < *(maxList + (keys + i)->index)) {
+            currentMaxValue = *(maxList + (keys + i)->index);
         }
-        unsigned char fill[3] = {122,122,122};
-        unsigned char border[3] = {255,255,255};
-        triangle(*screen, x, y,fill,border);
     }
-    free(keys);
+    zMask cullMask = ZMask(screen->length, screen->width);
+    for(int i = 0; i < object.amtOfTriangles - 1; i++) {
+        if(zCull[i] || (i != 0 && zCull[i-1])) {
+            int x[3];
+            int y[3];
+            float z[3];
+            for(int j = 0; j < 3; j++) {
+                x[j] = (int)(1000 * (*(object.verts+((*(object.triangles+((keys+i)->index)))[j])))[0]/(*(object.verts+((*(object.triangles+((keys+i)->index)))[j])))[2])+(screen->length>>1);
+                y[j] = (int)(1000 * (*(object.verts+((*(object.triangles+((keys+i)->index)))[j])))[1]/(*(object.verts+((*(object.triangles+((keys+i)->index)))[j])))[2])+(screen->width>>1);
+                z[j] = (*(object.verts+((*(object.triangles+((keys+i)->index)))[j])))[2];
+            }
+            unsigned char fill[3];
+            unsigned char border[3];
+            
+            for(int j = 0; j < 3; j++) {
+                fill[j] = object.fill[j];
+                border[j] = object.border[j];
+            }
+            zDrawTriangle(cullMask, screen, x, y, z, fill,border);
+        } else {
+            int x[3];
+            int y[3];
+            for(int j = 0; j < 3; j++) {
+                x[j] = (int)(1000 * (*(object.verts+((*(object.triangles+((keys+i)->index)))[j])))[0]/(*(object.verts+((*(object.triangles+((keys+i)->index)))[j])))[2])+(screen->length>>1);
+                y[j] = (int)(1000 * (*(object.verts+((*(object.triangles+((keys+i)->index)))[j])))[1]/(*(object.verts+((*(object.triangles+((keys+i)->index)))[j])))[2])+(screen->width>>1);
+            }
+            unsigned char fill[3];
+            unsigned char border[3];
+            
+            for(int j = 0; j < 3; j++) {
+                fill[j] = object.fill[j];
+                border[j] = object.border[j];
+            }
+            triangle(*screen, x, y,fill,border);
+        }
+    }
+    free(keys); free(maxList); free(cullMask.distances);
 };
 void addToScreen(int amtOfObjects, instance *instances, ray camera, scr *screen) {
     /* convert to objects */
@@ -141,12 +180,20 @@ void addToScreen(int amtOfObjects, instance *instances, ray camera, scr *screen)
 
     key *keyList = (key *)malloc(amtOfObjects*sizeof(key));
     o = objList;
+    float *minValList = (float *)malloc(amtOfObjects*sizeof(float));
     {
         for(int i = 0; i < amtOfObjects; i++) {
             (keyList + i)->index = i;
-            (keyList + i)->val = *(o->verts)[0];
-            for(int j = 1; j < o->amtOfVerts; j++) {
-                if(*(o->verts+j)[0] > (keyList + i)->val) (keyList + i)->val = *(o->verts+j)[0];
+            (keyList + i)->val = *((o+i)->verts)[0];
+            *(minValList+i) = *((o+i)->verts)[0];
+            for(int j = 1; j < (o+i)->amtOfVerts; j++) {
+                if(*((o+i)->verts+j)[0] > (keyList + i)->val) 
+
+                (keyList + i)->val = *((o+i)->verts+j)[0];
+
+                else if(*((o+i)->verts+j)[0] < (keyList + i)->val)
+
+                *(minValList+i) = *((o+i)->verts+j)[0];
             }
         }
         sort(amtOfObjects, keyList);
@@ -155,12 +202,13 @@ void addToScreen(int amtOfObjects, instance *instances, ray camera, scr *screen)
     key *k = keyList;
     int start = 0;
     while(start < amtOfObjects && (k++)->val < 0) start++;
+    k--;
     for(int i = start; i < amtOfObjects; i++) {
 
         addObject(*(objList+(k++)->index),screen);
     }
     /* free allocated data */
-    free(keyList);
+    free(keyList); free(minValList);
     o = objList;
     for(int i = 0; i < amtOfObjects; i++) {
         freeUp(*(o++));
